80dd572dea09313dce5d15467c9759d22fabc866
xiaoguo <740644717@qq.com>
2019-07-29 18:18:30 +0800

    update

--- app/Controller/MenuController.php
+++ app/Controller/MenuController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Exception\InvalidArgumentsException;
+use App\Validate\MenuValidate;
 use Hyperf\HttpServer\Annotation\AutoController;
 
 /**
@@ -24,6 +26,39 @@ class MenuController extends Controller
      */
     public function add()
     {
+        try {
+            if (!$this->isPost()) {
+                throw new \Exception('invalid access', 200);
+            }
+
+            $id = $this->request->post('id', '');
+
+            $data = [
+                'id' => $id,
+            ];
+
+            $method = __FUNCTION__;
+            $validate = new MenuValidate();
+            if (!$validate->scene($method)->check($data)) {
+                throw new InvalidArgumentsException($validate->getError(), 200);
+            }
+
+            //TODO 该角色下是否存在用户
+
+//            $service = new AuthService();
+//
+//            $res = $service->$method($id);
+//
+//            if (false === $res) {
+//                throw new \Exception('删除权限失败！', 200);
+//            }
+
+            return $this->response->success([], '添加成功！');
+        } catch (InvalidArgumentsException $exception) {
+            return $this->response->fail($exception->getCode(), $exception->getMessage());
+        } catch (\Exception $exception) {
+            return $this->response->fail($exception->getCode(), $exception->getMessage());
+        }
     }
 
     /**
