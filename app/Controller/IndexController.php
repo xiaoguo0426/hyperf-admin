b6c9d955d19172b9d1784d03c7242494ab40a2cf
xiaoguo0426 <740644717@qq.com>
2020-04-23 15:40:36 +0800

    update

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Logic\SettingLogic;
+use App\Util\Redis;
+use App\Util\RedisLock;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\SuperGlobals\Proxy\Get;
 use Psr\Http\Message\ServerRequestInterface;
@@ -33,11 +35,49 @@ class IndexController extends Controller
     public function index(ServerRequestInterface $request): \Psr\Http\Message\ResponseInterface
     {
 
+        $key = '2020';
+        $a = 'bbb';
+        try {
+            $cid = \Swoole\Coroutine::getCid();
+            echo 'Cid:' . $cid . PHP_EOL;
+
+            RedisLock::lock($key);
+            $redis = Redis::getInstance();
+            $res = $redis->incr($a);
+            RedisLock::release($key);
+            echo 'Cid:' . $cid . ' done';
+
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
+
         $di = di(SettingLogic::class);
 
         $setting = $di->getWeb();
 
         return $this->response->success($setting);
+
+    }
+
+    public function testLock()
+    {
+        $key = '2020';
+        $a = 'bbb';
+        try {
+            $cid = \Swoole\Coroutine::getCid();
+            echo 'Cid:' . $cid . PHP_EOL;
+
+            RedisLock::lock($key);
+            $redis = Redis::getInstance();
+            $res = $redis->incr($a);
+            var_dump($res);
+            RedisLock::release($key);
+            echo 'Cid:' . $cid . ' done';
+
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
+
 
     }
 
--- app/Listener/BootApplicationListener.php
+++ app/Listener/BootApplicationListener.php
@@ -27,8 +27,6 @@ class BootApplicationListener implements ListenerInterface
      */
     public function process(object $event)
     {
-        var_dump(123123123123);
-
         // TODO: Implement process() method.
         if ($event instanceof BootApplication) {
             Sentry\init(['dsn' => 'http://6ad3bdc5904a4f909b81e379c73ff92f@sentry-web:9000/2']);
